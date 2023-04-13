54875fa971e59fff91ea9e3113467193a202d027
snehal kanase <snehalkanase90@gmail.com>
2023-04-13 11:41:14 +0530

    added new functionality

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 const buttonc = document.querySelectorAll('#button');
 const rock = document.getElementById("rock");
-const pepar = document.getElementById("pepar");
+const paper = document.getElementById("paper");
 const scissor = document.getElementById("scissor");
 const res = document.getElementById("res");
+const pScore = document.getElementById('pScore');
+const cScore = document.getElementById('cScore');
+const final = document.getElementById('final');
+const restart = document.getElementById('restart');
 let playerChoice = document.getElementById('playerSelection');
 let compChoice = document.getElementById('computerSelection');
 let playerScore = 0;
 let computerScore = 0;
 
 rock.addEventListener('click', () => handleClick("Rock"));
-pepar.addEventListener('click', () => handleClick("Pepar"));
+paper.addEventListener('click', () => handleClick("Paper"));
 scissor.addEventListener('click', () => handleClick("Scissor"));
 
+restart.addEventListener('click', function restart() {
+    location.reload();
+ });
+
 function handleClick(playerSelection) {
+
     playerChoice.innerHTML = playerSelection;
     const computerSelection = ComputerChoice();
     compChoice.innerHTML = computerSelection;
-    res.innerHTML = playRound(playerSelection, computerSelection)
+    res.innerHTML = playRound(playerSelection, computerSelection);
+    updateScore();
+
+    if (gameOver()) {
+        final.innerHTML = finalMsg()
+    }
 }
 function ComputerChoice() {
     const num = Math.floor(Math.random() * 3) + 1;
@@ -26,7 +40,7 @@ function ComputerChoice() {
         case 1:
             return "Rock";
         case 2:
-            return "Pepar";
+            return "Paper";
         case 3:
             return "Scissor";
         default:
@@ -36,21 +50,64 @@ function ComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        playerScore++;
-        computerScore++;
+        // playerScore++;
+        // computerScore++;
+
         return "draw"
     } else if (playerSelection == "Rock" && computerSelection == "Scissor" ||
-               playerSelection == "Pepar" && computerSelection == "Rock" ||
-               playerSelection == "Scissor" && computerSelection == "Pepar"){
+        playerSelection == "Paper" && computerSelection == "Rock" ||
+        playerSelection == "Scissor" && computerSelection == "Paper") {
 
-                playerScore++;
-                return `You win the round!! ${playerSelection} beats the ${computerSelection} `
+        playerScore++;
+        return `You win the round!! ${playerSelection} beats the ${computerSelection} `
     }
     else if (computerSelection == "Rock" && playerSelection == "Scissor" ||
-               computerSelection == "Pepar" && playerSelection == "Rock" ||
-               computerSelection == "Scissor" && playerSelection == "Pepar"){
-         
-                computerScore++;
-                return `Computer win the Round!! ${computerSelection} beats the ${playerSelection}`
+        computerSelection == "Paper" && playerSelection == "Rock" ||
+        computerSelection == "Scissor" && playerSelection == "Paper") {
+
+        computerScore++;
+        return `Computer win the Round!! ${computerSelection} beats the ${playerSelection}`
+    }
+
 }
+
+function updateScore() {
+    pScore.textContent = `Player Score:${playerScore}`;
+    cScore.innerHTML = `Computer Score:${computerScore}`;
+    console.log(playerScore)
+}
+
+function gameOver() {
+    return playerScore === 5 || computerScore === 5;
+}
+
+function finalMsg() {
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === computerScore) {
+            rock.disabled = true;
+            paper.disabled = true;
+            scissor.disabled = true;
+
+            computerScore = 0;
+            playerScore = 0;
+            return "draw"
+        } else if (playerScore > computerScore) {
+            rock.disabled = true;
+            paper.disabled = true;
+            scissor.disabled = true;
+
+            computerScore = 0;
+            playerScore = 0;
+            return " You win the match"
+        } else if (playerScore < computerScore) {
+            rock.disabled = true;
+            paper.disabled = true;
+            scissor.disabled = true;
+
+            computerScore = 0;
+            playerScore = 0;
+            return "Computer win the match"
+        }
+
+    }
 }
